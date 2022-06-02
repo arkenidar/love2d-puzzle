bf2c89a07396ff156e84d171631687d124a946ce
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-06-02 13:46:02 +0200

    game rule: swaps with empty when possible. draws the squares. small window.

--- main.lua
+++ main.lua
@@ -1,4 +1,17 @@
 
+-- 8 puzzle
+
+function love.load()
+  size = 3
+  table_grid={
+   {1,2,3},
+   {4,5,6},
+   {7,8,0},
+  }
+  side = 50
+  love.window.setMode(size*side,size*side)
+end
+
 function point_in_rectangle(point,xywh)
   return
     point[1]>=xywh[1] and
@@ -7,17 +20,8 @@ function point_in_rectangle(point,xywh)
     point[2]<=(xywh[2]+xywh[4])
 end
 
-local table_grid={
- {1,2,3},
- {4,5,6},
- {7,8,0},
-}
-
 function love.draw()
-  
-  local side = 50
-  local center = side/2
-  
+
   for row_id , row in pairs(table_grid) do
     for column_id , element in pairs(row) do
       local x_id , y_id
@@ -29,15 +33,61 @@ function love.draw()
       ry = y_id*side
       
       local mx , my = love.mouse.getPosition()
-      
-      if point_in_rectangle({mx,my} , {rx,ry,side,side} ) then
-         love.graphics.setColor(1,0,0)
-      else
-         love.graphics.setColor(1,1,1)
+      local square = {rx,ry,side,side}
+      local inside = point_in_rectangle({mx,my} , square )
+    
+      function swap(row_id_2, column_id_2)
+        local temporary
+        temporary = table_grid[row_id][column_id]
+        table_grid[row_id][column_id] = table_grid[row_id_2][column_id_2]
+        table_grid[row_id_2][column_id_2] = temporary
+      end
+    
+      if inside and love.mouse.isDown(1) then
+
+        -- left
+        if column_id > 1 and table_grid[row_id][column_id - 1] == 0 then
+          swap(row_id, column_id - 1)
+        end
+        
+        -- right
+        if column_id < size and table_grid[row_id][column_id + 1] == 0 then
+          swap(row_id, column_id + 1)
+        end
+        
+        -- up
+        if row_id > 1 and table_grid[row_id - 1][column_id] == 0 then
+          swap(row_id - 1, column_id)
+        end
+        
+        -- down
+        if row_id < size and table_grid[row_id + 1][column_id] == 0 then
+          swap(row_id + 1, column_id)
+        end
+        
       end
       
-      love.graphics.print(element, -- content
-        rx+center, ry+center) -- x y
+      
+      love.graphics.setColor(0,0,0) -- black
+      love.graphics.rectangle("fill", square[1], square[2], square[3], square[4])
+      
+      if inside then
+        love.graphics.setColor(1,0,0) -- red
+      else
+        love.graphics.setColor(1,1,1) -- white
+      end
+      love.graphics.rectangle("line", square[1]+1, square[2]+1, square[3]-2, square[4]-2)
+      
+      if element > 0 then
+        local center = side/2
+        local font = love.graphics.getFont()
+        local font_height = font:getHeight()
+        local font_width = font:getWidth(tostring(element))
+        local x,y
+        x = rx + center - font_width/2
+        y = ry + center - font_height/2
+        love.graphics.print(element, x , y)
+      end
       
     end -- end for/row
   end -- end for/table
