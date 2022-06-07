2bcac5fa23b4f42caaca5eecc2a309b1b17502ed
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-06-07 11:51:11 +0200

    configure: console = true

--- conf.lua
+++ conf.lua
@@ -1,4 +1,7 @@
 function love.conf(t)
   -- https://love2d.org/forums/viewtopic.php?t=84826
   t.window.fullscreen = love._os == "Android" or love._os == "iOS"
+  
+  -- https://love2d.org/forums/viewtopic.php?t=79579
+  t.console = true
 end
